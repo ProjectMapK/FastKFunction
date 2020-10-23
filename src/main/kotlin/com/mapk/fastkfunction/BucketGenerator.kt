81333b4c3c15e80b04bde247088e2d9c9b447ab1
wrongwrong <boranti1995@gmail.com>
2020-10-24 00:25:50 +0900

    バケツジェネレータの追加

---
+++ src/main/kotlin/com/mapk/fastkfunction/BucketGenerator.kt
@@ -0,0 +1,18 @@
+package com.mapk.fastkfunction
+
+import kotlin.reflect.KParameter
+
+internal class BucketGenerator(private val parameters: List<KParameter>, instance: Any?) {
+    private val originalValueArray: Array<Any?> = Array(parameters.size) { null }
+    private val originalInitializationStatuses: BooleanArray = BooleanArray(parameters.size)
+
+    init {
+        if (instance != null) {
+            originalValueArray[0] = instance
+            originalInitializationStatuses[0] = true
+        }
+    }
+
+    fun generateBucket(): ArgumentBucket =
+        ArgumentBucket(parameters, originalValueArray.clone(), originalInitializationStatuses.clone())
+}
