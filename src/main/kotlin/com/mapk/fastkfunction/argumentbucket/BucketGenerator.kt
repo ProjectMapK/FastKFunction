ba9ea8dae68f2d3b8f7d0821250fb51f51a732e6
wrongwrong <boranti1995@gmail.com>
2020-10-31 22:37:45 +0900

    バケツ生成方法を調整

--- src/main/kotlin/com/mapk/fastkfunction/argumentbucket/BucketGenerator.kt
+++ src/main/kotlin/com/mapk/fastkfunction/argumentbucket/BucketGenerator.kt
@@ -2,17 +2,20 @@ package com.mapk.fastkfunction.argumentbucket
 
 import kotlin.reflect.KParameter
 
-internal class BucketGenerator(private val parameters: List<KParameter>, instance: Any?) {
+internal class BucketGenerator(private val parameters: List<KParameter>) {
     private val originalValueArray: Array<Any?> = Array(parameters.size) { null }
     private val originalInitializationStatuses: BooleanArray = BooleanArray(parameters.size)
+    private val valueArrayGetter: (Array<Any?>) -> Array<Any?>
 
     init {
-        if (instance != null) {
-            originalValueArray[0] = instance
+        if (parameters[0].kind != KParameter.Kind.VALUE) {
             originalInitializationStatuses[0] = true
+            valueArrayGetter = { it.copyOfRange(1, parameters.size) }
+        } else {
+            valueArrayGetter = { it }
         }
     }
 
     fun generateBucket(): ArgumentBucket =
-        ArgumentBucket(parameters, originalValueArray.clone(), originalInitializationStatuses.clone())
+        ArgumentBucket(parameters, originalValueArray.clone(), originalInitializationStatuses.clone(), valueArrayGetter)
 }
