b8616d9ba6b928dda8bae83eb353e3e990ff5830
wrongwrong <boranti1995@gmail.com>
2020-11-21 14:47:18 +0900

    インスタンスを取れなくなっていた問題を修正

--- src/main/kotlin/com/mapk/fastkfunction/argumentbucket/BucketGenerator.kt
+++ src/main/kotlin/com/mapk/fastkfunction/argumentbucket/BucketGenerator.kt
@@ -2,7 +2,7 @@ package com.mapk.fastkfunction.argumentbucket
 
 import kotlin.reflect.KParameter
 
-internal class BucketGenerator(private val parameters: List<KParameter>) {
+internal class BucketGenerator(private val parameters: List<KParameter>, instance: Any?) {
     private val originalValueArray: Array<Any?> = Array(parameters.size) { null }
     private val originalInitializationStatuses: BooleanArray = BooleanArray(parameters.size)
     private val valueArrayGetter: (Array<Any?>) -> Array<Any?>
@@ -10,6 +10,7 @@ internal class BucketGenerator(private val parameters: List<KParameter>) {
     init {
         if (parameters[0].kind != KParameter.Kind.VALUE) {
             originalInitializationStatuses[0] = true
+            originalValueArray[0] = instance
             valueArrayGetter = { it.copyOfRange(1, parameters.size) }
         } else {
             valueArrayGetter = { it }
