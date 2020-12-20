f129f62355e0497d81d18b1b59152b865142bfd7
wrongwrong <boranti1995@gmail.com>
2020-12-20 22:17:16 +0900

    instanceパラメータバリデーション時の「Nullならthrow」の共通化用関数を定義

--- src/main/kotlin/com/mapk/fastkfunction/InternalFunctions.kt
+++ src/main/kotlin/com/mapk/fastkfunction/InternalFunctions.kt
@@ -1,6 +1,7 @@
 package com.mapk.fastkfunction
 
 import java.lang.reflect.Method
+import kotlin.reflect.KParameter
 
 /**
  * Get object instance if receiver declared in object.
@@ -10,3 +11,14 @@ import java.lang.reflect.Method
  * @throws UnsupportedOperationException Method declared on top level.
  */
 internal val Method.declaringObject: Any? get() = declaringClass.kotlin.objectInstance
+
+/**
+ * Throw IllegalArgumentException if instance is null.
+ *
+ * @receiver Instance parameter.
+ * @param kind Instance Kind.
+ * @return instance.
+ * @throws IllegalArgumentException Instance is null.
+ */
+internal fun <T : Any> T?.instanceOrThrow(kind: KParameter.Kind): T =
+    this ?: throw IllegalArgumentException("Function requires ${kind.name} parameter, but is not present.")
