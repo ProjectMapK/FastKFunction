6ba6702feeb421b0b220268df11ad442c70cc401
wrongwrong <boranti1995@gmail.com>
2021-01-11 14:44:08 +0900

    判定をABSENT_VALUEで書き直し

--- src/main/kotlin/com/mapk/fastkfunction/argumentbucket/ArgumentBucket.kt
+++ src/main/kotlin/com/mapk/fastkfunction/argumentbucket/ArgumentBucket.kt
@@ -6,74 +6,58 @@ import kotlin.reflect.KParameter
 class ArgumentBucket(
     private val keyList: List<KParameter>,
     private val valueArray: Array<Any?>,
-    private val initializationStatuses: BooleanArray,
+    private var count: Int,
     private val valueArrayGetter: (Array<Any?>) -> Array<Any?>
 ) : Map<KParameter, Any?> {
-    fun isFullInitialized(): Boolean = initializationStatuses.all { it }
+    fun isFullInitialized(): Boolean = count == keyList.size
 
     // getValueArrayは内部処理でしか利用しないためinternal化
     internal fun getValueArray(): Array<Any?> = valueArrayGetter(valueArray)
 
-    operator fun set(key: KParameter, value: Any?): Any? {
-        return valueArray[key.index].apply {
-            valueArray[key.index] = value
-            initializationStatuses[key.index] = true
-        }
+    operator fun set(key: KParameter, value: Any?): Any? = set(key.index, value)
+
+    operator fun set(index: Int, value: Any?): Any? = valueArray[index].apply {
+        if (this === ABSENT_VALUE) count++
+        valueArray[index] = value
     }
 
-    operator fun set(index: Int, value: Any?): Any? {
-        return valueArray[index].apply {
+    /**
+     * If the specified key is not already associated with a value associates it with the given value and returns
+     * {@code null}, else returns the current value.
+     */
+    fun setIfAbsent(key: KParameter, value: Any?): Any? = setIfAbsent(key.index, value)
+
+    /**
+     * If the specified key is not already associated with a value associates it with the given value and returns
+     * {@code null}, else returns the current value.
+     */
+    fun setIfAbsent(index: Int, value: Any?): Any? = valueArray[index].apply {
+        if (this === ABSENT_VALUE) {
+            count++
             valueArray[index] = value
-            initializationStatuses[index] = true
         }
     }
 
-    /**
-     * If the specified key is not already associated with a value associates it with the given value and returns
-     * {@code null}, else returns the current value.
-     */
-    fun setIfAbsent(key: KParameter, value: Any?): Any? {
-        return if (initializationStatuses[key.index])
-            valueArray[key.index]
-        else
-            null.apply {
-                valueArray[key.index] = value
-                initializationStatuses[key.index] = true
-            }
-    }
-
-    /**
-     * If the specified key is not already associated with a value associates it with the given value and returns
-     * {@code null}, else returns the current value.
-     */
-    fun setIfAbsent(index: Int, value: Any?): Any? {
-        return if (initializationStatuses[index])
-            valueArray[index]
-        else
-            null.apply {
-                valueArray[index] = value
-                initializationStatuses[index] = true
-            }
-    }
+    private fun isInitialized(index: Int): Boolean = valueArray[index] !== ABSENT_VALUE
 
     override val entries: Set<Map.Entry<KParameter, Any?>>
         get() = keyList.fold(HashSet()) { acc, cur ->
-            acc.apply { if (initializationStatuses[cur.index]) add(Entry(cur, valueArray[cur.index])) }
+            acc.apply { if (isInitialized(cur.index)) add(Entry(cur, valueArray[cur.index])) }
         }
     override val keys: Set<KParameter>
         get() = keyList.fold(HashSet()) { acc, cur ->
-            acc.apply { if (initializationStatuses[cur.index]) add(cur) }
+            acc.apply { if (isInitialized(cur.index)) add(cur) }
         }
-    override val size: Int get() = initializationStatuses.count { it }
+    override val size: Int get() = count
     override val values: Collection<Any?>
-        get() = valueArray.filterIndexed { idx, _ -> initializationStatuses[idx] }
+        get() = valueArray.filter { it !== ABSENT_VALUE }
 
     // keyはインスタンスの一致と初期化状態を見る
     override fun containsKey(key: KParameter): Boolean =
-        keyList[key.index] === key && initializationStatuses[key.index]
+        keyList[key.index] === key && isInitialized(key.index)
 
     override fun containsValue(value: Any?): Boolean = valueArray.withIndex()
-        .any { initializationStatuses[it.index] && it.value == value }
+        .any { isInitialized(it.index) && it.value == value }
 
     override fun get(key: KParameter): Any? = valueArray[key.index]
     operator fun get(index: Int): Any? = valueArray[index]
